b027140d328e5389cd5e1decb17674c56fb35387
Oleg G.Kapranov <oleg@vertiopia.com>
2017-01-18 12:25:49 +0200

    added redis cache sessions

--- strings-server/config/environments/development.rb
+++ strings-server/config/environments/development.rb
@@ -3,22 +3,24 @@ Rails.application.configure do
   config.eager_load = false
   config.consider_all_requests_local = true
 
-  if Rails.root.join('tmp/caching-dev.txt').exist?
+  if ENV['REDIS_URL']
     config.action_controller.perform_caching = true
-
-    config.cache_store = :memory_store
-    config.public_file_server.headers = {
-      'Cache-Control' => 'public, max-age=172800'
-    }
+    config.cache_store = :redis_store, Rails.application.secrets.redis_url, { expires_in: 90.minutes }
   else
     config.action_controller.perform_caching = false
-
     config.cache_store = :null_store
   end
 
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
   config.active_support.deprecation = :log
-  # config.action_view.raise_on_missing_translations = true
-  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
+
+# config.action_mailer.delivery_method = :smtp
+# config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
+# config.action_mailer.default_url_options = { :host => 'localhost', port: '3000' }
+#
+# config.action_dispatch.tld_length = 1
+#
+# config.web_console.automount = true
+# config.web_console.whitelisted_ips =  %w( 127.0.0.1 )
---
+++ strings-server/config/initializers/redis.rb
@@ -0,0 +1,1 @@
+$redis = Redis::Namespace.new("strings_server", :redis => Redis.new)
