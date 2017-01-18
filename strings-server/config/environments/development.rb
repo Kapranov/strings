890e494b2629ce3cf2d6c033d587a3bb02466b14
Oleg G.Kapranov <oleg@vertiopia.com>
2017-01-18 17:08:10 +0200

    letter_opener for mail

--- strings-server/config/environments/development.rb
+++ strings-server/config/environments/development.rb
@@ -11,9 +11,7 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
-  # config.action_mailer.default_url_options = { :host => 'localhost', port: '3000' }
+  config.action_mailer.delivery_method = :letter_opener
   config.action_dispatch.tld_length = 1
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
