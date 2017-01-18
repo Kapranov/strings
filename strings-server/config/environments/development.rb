57601e0c625d496e5973bf10c167f6d9cd96240a
Oleg G.Kapranov <oleg@vertiopia.com>
2017-01-18 13:08:32 +0200

    added web-console

--- strings-server/config/environments/development.rb
+++ strings-server/config/environments/development.rb
@@ -11,17 +11,13 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
+  # config.action_mailer.default_url_options = { :host => 'localhost', port: '3000' }
   config.action_dispatch.tld_length = 1
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
   config.active_support.deprecation = :log
+  config.web_console.automount = true
+  config.web_console.whitelisted_ips = Rails.application.secrets.host_ip
 end
-
-# config.action_mailer.delivery_method = :smtp
-# config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
-# config.action_mailer.default_url_options = { :host => 'localhost', port: '3000' }
-#
-# config.action_dispatch.tld_length = 1
-#
-# config.web_console.automount = true
-# config.web_console.whitelisted_ips =  %w( 127.0.0.1 )
